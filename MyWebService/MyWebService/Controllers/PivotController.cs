3336941d474a8ee9553c6ca69715b80a4b9e22b0
Mohanraj Elumalai <mohanraj.elumalai@syncfusion.com>
2023-03-24 18:59:27 +0530

    feature(BLAZ-30173): Oracle database samples in blazor pivot table component.

---
+++ MyWebService/MyWebService/Controllers/PivotController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
+
+namespace MyWebService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PivotController : ControllerBase
+    {
+        [HttpGet(Name = "GetOracleDbResult")]
+        public object Get()
+        {
+            return JsonConvert.SerializeObject(FetchOracleDbResult());
+        }
+
+        private static DataTable FetchOracleDbResult()
+        {
+            // Replace with your own connection string.
+            string connectionString = "Data Source=(DESCRIPTION =" +
+            "(ADDRESS = (PROTOCOL = TCP)(HOST = 172.16.200.86)(PORT = 1521))" +
+            "(CONNECT_DATA =" + "(SERVICE_NAME = ORCL)));" + "User Id=SYSTEM;Password=coolcomp@123;";
+            OracleConnection oracleConnection = new OracleConnection(connectionString);
+            oracleConnection.Open();
+            OracleCommand command = new OracleCommand("SELECT * FROM EMPLOYEES", oracleConnection);
+            OracleDataAdapter dataAdapter = new OracleDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            oracleConnection.Close();
+            return dataTable;
+        }
+    }
+}
